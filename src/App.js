423787b96ef36b584d588d304125e233f54da0b2
Dylan Cooper <dylancooper155@gmail.com>
2020-10-13 12:11:15 -0400

    refactored IssueList.jsx to use hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Issue from "./components/Issue";
+import { BrowserRouter as Router } from "react-router-dom";
+import IssueList from "./components/IssuesList";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Issue/>
+      <Router>
+      <IssueList/>
+      </Router>
       </header>
     </div>
   );
--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import IssuesList from "./IssuesList";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { useParams } from "react-router-dom";
 
+const Issue = (props) => {
+  const { issues } = props;
+  const { issue_number } = useParams();
+  const issue = issues.find((issue) => {
+    return issue.number === parseInt(issue_number) ? issue : null;
+  })
+  return (
+  <>
+    <h2>{issue.title}</h2>
+    {/* <p>
+      <a href={issue.url}>{issue.url}</a>
+    </p> */}
+    
+    <ReactMarkdown source={issue.body} escapeHtml={false} />
+    
+  </>
 
-
-const Issue = props => {
-    return (
-        <IssuesList />
-    )
-
+  );
 }
-
 
 export default Issue;
